ec29e0f857d2b2b2855bcdb311e3d391908cbf7c
Vardhan-R <82092439+Vardhan-R@users.noreply.github.com>
2024-07-07 22:52:22 +0530

    Update reset.ps1

--- reset.ps1
+++ reset.ps1
@@ -1,5 +1,5 @@
 Remove-Item ".\images" -Recurse -Force
 Remove-Item ".\data.csv" -Force
 Remove-Item ".\did_not_save.csv" -Force
-Remove-Item ".\next_post_id.txt" -Force
 Remove-Item ".\communication.txt" -Force
+Remove-Item ".\next_post_id.txt" -Force
